67bbff3577783ba8615138789e46397842f6cb5f
SkyAo <csvwolf@qq.com>
2020-09-26 22:07:20 +0800

    chore: auto fix and lint monky scripts (#5)

--- monkey-scripts/event-guard.js
+++ monkey-scripts/event-guard.js
@@ -13,43 +13,43 @@
 // ==/UserScript==
 
 (function() {
-    'use strict'
-    const notice = function (ok, word, reloadInterval) {
-        if (ok) {
-            let shaking = false
-            setInterval(() => {
-                if (shaking) document.title = `[Notification] ${word}!!`
-                else document.title = word
-                shaking = !shaking
-            }, 200)
-        } else {
-            setTimeout(() => {
-                window.location.reload()
-            }, reloadInterval)
-        }
+  'use strict'
+  const notice = function(ok, word, reloadInterval) {
+    if (ok) {
+      let shaking = false
+      setInterval(() => {
+        if (shaking) document.title = `[Notification] ${word}!!`
+        else document.title = word
+        shaking = !shaking
+      }, 200)
+    } else {
+      setTimeout(() => {
+        window.location.reload()
+      }, reloadInterval)
     }
+  }
 
-    const path = window.location.pathname
-    const isTreasure = path.includes('/freebies/tarlastoolbar.phtml')
-    const isMagma = path.includes('/magma/pool.phtml')
-    const isAttic = path.includes('/halloween/garage.phtml')
+  const path = window.location.pathname
+  const isTreasure = path.includes('/freebies/tarlastoolbar.phtml')
+  const isMagma = path.includes('/magma/pool.phtml')
+  const isAttic = path.includes('/halloween/garage.phtml')
 
-    if (isTreasure) {
-        const notHere = document.querySelector('.content').innerText.includes(`Check back later to see if she's here!`)
-        notice(!notHere, 'Tarla is here now', 5000)
+  if (isTreasure) {
+    const notHere = document.querySelector('.content').innerText.includes('Check back later to see if she\'s here!')
+    notice(!notHere, 'Tarla is here now', 5000)
+  }
+
+  if (isMagma) {
+    const notHere = document.querySelector('.content').innerText.includes('Learn more and try again later.')
+    notice(!notHere, 'Magma pool is open', 1000 * 20)
+    // Magma Pool time record to console
+    if (!notHere) {
+      console.info(new Date())
     }
+  }
 
-    if (isMagma) {
-        const notHere = document.querySelector('.content').innerText.includes(`Learn more and try again later.`)
-        notice(!notHere, 'Magma pool is open', 1000 * 20)
-        // Magma Pool time record to console
-        if (!notHere) {
-            console.info(new Date())
-        }
-    }
-
-    if (isAttic) {
-        const notHere = document.querySelector('.content').innerText.includes(`Oops`)
-        notice(!notHere, 'Attic has stocks', 10000)
-    }
+  if (isAttic) {
+    const notHere = document.querySelector('.content').innerText.includes('Oops')
+    notice(!notHere, 'Attic has stocks', 10000)
+  }
 })()
