4f7d232a834bbc31607609aa4ef037948f54d5d8
SkyAo <csvwolf@qq.com>
2020-09-26 22:03:55 +0800

    feat: vue3 init (#4)

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  >
+  <HelloWorld msg="Hello Neopets" />
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
