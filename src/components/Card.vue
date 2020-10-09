58dfa6853644d84245be8da299336b4a300b7428
SkyAo <csvwolf@qq.com>
2020-10-09 22:12:26 +0800

    feat: add card component (#9)

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,3 +1,43 @@
 <template>
-  <div />
+  <section class="card">
+    <img
+      class="card__img"
+      :src="image"
+    >
+    <div class="card__title">
+      {{ title }}
+    </div>
+  </section>
 </template>
+<style scoped lang="postcss">
+.card {
+  width: 200px;
+  height: 80px;
+  display: inline-block;
+
+  &__img {
+    width: 100px;
+    height: auto;
+  }
+
+  &__title {
+    font-size: 16px;
+  }
+}
+</style>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  }
+})
+</script>
