8250d2d4f8c6f164820c3a9c9c1df09dd2529f7c
SkyAo <csvwolf@qq.com>
2020-10-09 22:11:27 +0800

    feat: add card component

--- src/pages/daily/index.vue
+++ src/pages/daily/index.vue
@@ -1,7 +1,28 @@
 <template>
   <div>
     <ul>
-      <li>Blue Grundo</li>
+      <li>
+        <card
+          :image="blueGrundo"
+          title="Blue Grundo"
+        />
+      </li>
     </ul>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Card from '@components/Card.vue'
+import BlueGrundo from '@/assets/daily/blue-grundo.gif'
+
+export default defineComponent({
+  components: {
+    Card
+  },
+  setup() {
+    return {
+      blueGrundo: BlueGrundo
+    }
+  }
+})
+</script>
