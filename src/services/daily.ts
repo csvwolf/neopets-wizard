7d41dd19bf52c89bdaa0820f62b1e536795f1531
SkyAo <csvwolf@qq.com>
2020-12-13 16:04:09 +0800

    feat: add shop of offer and trudy surprise

--- src/services/daily.ts
+++ src/services/daily.ts
@@ -2,8 +2,18 @@ import http from './basic'
 
 export const runAutoBlueGrundoGift = () => {
   return http.post('/daily/grundo', {
-    data: {
-      session: localStorage.getItem('cookie')
-    }
+    session: localStorage.getItem('cookie')
+  })
+}
+
+export const runAutoShopOfOffer = () => {
+  return http.post('/daily/shop-of-offer', {
+    session: localStorage.getItem('cookie')
+  })
+}
+
+export const runAutoTrudysSurprise = () => {
+  return http.post('/daily/trudys-surprise', {
+    session: localStorage.getItem('cookie')
   })
 }
